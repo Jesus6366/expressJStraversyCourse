ecab678d70444468442dbd60c1485c0ce9aeedb8
unknown <polibos6366@gmail.com>
2024-09-08 15:07:46 -0600

    crud api

---
+++ routes/posts.js
@@ -0,0 +1,83 @@
+const express = require("express");
+
+const router = express.Router();
+
+let posts = [
+  { id: 1, title: "Post one" },
+  { id: 2, title: "Post 2" },
+  { id: 3, title: "Post 3" },
+];
+
+//json api GET all posts
+router.get("/", (req, res) => {
+  const limit = parseInt(req.query.limit);
+
+  if (!isNaN(limit) && limit > 0) {
+    return res.status(200).json(posts.slice(0, limit));
+  }
+
+  res.status(200).json(posts);
+});
+
+// Get a single post
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res
+      .status(404)
+      .json({ msg: `A post with the id of ${id} was not found` });
+  }
+  res.status(200).json(post);
+});
+
+// Create new post POST
+router.post("/", (req, res) => {
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+  };
+
+  if (!newPost.title) {
+    return res.status(400).json({ msg: "Please include a title" });
+  }
+  posts.push(newPost);
+
+  res.status(201).json(posts);
+});
+
+// UPDATE POST
+
+router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res
+      .status(404)
+      .json({ msg: `A post with the id of ${id} was not found` });
+  }
+
+  post.title = req.body.title;
+  res.status(200).json(posts);
+});
+
+//DELETE POSTS
+
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res
+      .status(404)
+      .json({ msg: `A post with the id of ${id} was not found` });
+  }
+
+  posts = posts.filter((post) => post.id !== id);
+
+  res.status(200).json(posts);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const posts = require("./routes/posts");
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -7,45 +8,11 @@ const app = express();
 //middlwares
 // setup static folder
 // app.use(express.static(path.join(__dirname, "public")));
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
-// app.get("/", (req, res) => {
-//   // send html files
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
-
-// app.get("/about", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "about.html"));
-// });
-
-let posts = [
-  { id: 1, title: "Post one" },
-  { id: 2, title: "Post 2" },
-  { id: 3, title: "Post 3" },
-];
-
-//json api GET all posts
-app.get("/api/posts", (req, res) => {
-  const limit = parseInt(req.query.limit);
-
-  if (!isNaN(limit) && limit > 0) {
-    return res.status(200).json(posts.slice(0, limit));
-  }
-
-  res.status(200).json(posts);
-});
-
-// Get a single post
-app.get("/api/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-
-  if (!post) {
-    return res
-      .status(404)
-      .json({ msg: `A post with the id of ${id} was not found` });
-  }
-  res.status(200).json(post);
-});
+app.use("/api/posts", posts);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
